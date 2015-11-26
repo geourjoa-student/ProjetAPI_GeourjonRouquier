9055c8d0441a47350e6fb6f68315dcefb4d038df
anthony <anthony@anthony-portable.unassigned-domain>
2015-11-26 09:33:38 +0100

    Initial Commit

---
+++ triangle.adb
@@ -0,0 +1,20 @@
+type Doublet ;
+
+type AdDoublet is access Doublet ;
+
+type Doublet is record
+	Triangle : T_Triangle ; 
+	Suc : AdDoublet ; -- pointeur sur un autre Doublet
+end record;
+
+type T_Coord is record
+	X : integer;
+	Y : integer;
+	Z : integer;
+end record;
+
+type T_Triangle is record
+	Coord_1 : T_Coord;
+	Coord_2 : T_Coord;
+	Coord_3 : T_Coord;
+end record;
